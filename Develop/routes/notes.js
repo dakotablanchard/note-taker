4048b155647e461a41074c4ce4a744bdbff983d0
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-12-02 16:09:08 -0500

    render active note works

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -10,4 +10,16 @@ notes.get('/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+notes.get('/:id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id === noteId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No note with that ID');
+      });
+  });
+
 module.exports = notes;
