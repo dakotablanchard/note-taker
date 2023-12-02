6b1fff837871cb07ed6d38ca4f32b0ad98d068d0
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-12-02 15:20:13 -0500

    server running, loads index.html and notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const app = express();
 
 // Specify on which port the Express.js server will run
-const PORT = processs.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 // ----- MIDDLEWARE -----
 // Static middleware pointing to the public folder
@@ -17,3 +17,17 @@ app.use(express.static('public'));
 app.use(express.json());
 // Middleware to parse URL-encoded request bodies
 app.use(express.urlencoded({ extended: true }));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for homepage
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
