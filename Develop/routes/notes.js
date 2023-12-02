56bbb60939e67ee6cedc0b0e5e6f96e3388c8db3
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-12-02 16:36:12 -0500

    working on post route for posting notes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,4 +1,5 @@
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 const {
   readFromFile,
   readAndAppend,
@@ -10,16 +11,35 @@ notes.get('/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-notes.get('/:id', (req, res) => {
-    const noteId = req.params.note_id;
-    readFromFile('./db/db.json')
-      .then((data) => JSON.parse(data))
-      .then((json) => {
-        const result = json.filter((note) => note.id === noteId);
-        return result.length > 0
-          ? res.json(result)
-          : res.json('No note with that ID');
-      });
+//GET Route for specific note
+// notes.get('/notes/:id', (req, res) => {
+//     const noteId = req.params.note_id;
+//     readFromFile('./db/db.json')
+//       .then((data) => JSON.parse(data))
+//       .then((json) => {
+//         const result = json.filter((note) => note.id === noteId);
+//         return result.length > 0
+//           ? res.json(result)
+//           : res.json('No note with that ID');
+//       });
+//   });
+
+//  POST Route for note
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, '../db/db.json');
+      res.json(`Note added successfully`);
+    } else {
+      res.error('Error in adding note');
+    }
   });
 
 module.exports = notes;
